07ddb64bbe27fcda5adf08dcc95c2238869a0883
little-tonii <laptrinhcungtony@gmail.com>
2025-03-29 14:22:28 +0700

    update

--- gateway/src/services/category_service.py
+++ gateway/src/services/category_service.py
@@ -2,14 +2,15 @@ from fastapi import HTTPException
 import httpx
 import pendulum
 from starlette import status
-
+from datetime import datetime
 from ..models.user_model import AccountType
 
 from ..configs.variables import PRODUCT_SERVICE_URL
 from ..schemas.responses.category_response_schema import CreateCategoryResponse, GetAllCategoryResponse, GetCategoryResponse
+from typing import cast
 
 class CategoryService:
-    
+
     @classmethod
     async def get_all_categories(cls) -> GetAllCategoryResponse:
         try:
@@ -21,17 +22,17 @@ class CategoryService:
                         GetCategoryResponse(
                             id=category["id"],
                             name=category["name"],
-                            created_at=pendulum.parse(category["created_at"]),
-                            updated_at=pendulum.parse(category["updated_at"])
+                            created_at=cast(datetime, pendulum.parse(category["created_at"])),
+                            updated_at=cast(datetime, pendulum.parse(category["updated_at"]))
                         )
                         for category in response.json()
                     ]
                 )
-        except httpx.HTTPStatusError as e:
+        except httpx.HTTPStatusError:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Có lỗi xảy ra phía dịch vụ sản phẩm")
         except (httpx.ConnectError, httpx.TimeoutException, httpx.RequestError):
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Dịch vụ sản phẩm không khả dụng')
-    
+
     @classmethod
     async def create_category(cls, account_type: str, name: str) -> CreateCategoryResponse:
         if account_type != AccountType.ADMIN:
@@ -40,14 +41,15 @@ class CategoryService:
             async with httpx.AsyncClient() as client:
                 response = await client.post(f"{PRODUCT_SERVICE_URL}/category/", json={ "name": name })
                 response.raise_for_status()
+                created_at = cast(datetime, pendulum.parse(response.json()["created_at"]))
+                updated_at = cast(datetime, pendulum.parse(response.json()["updated_at"]))
                 return CreateCategoryResponse(
                     id=response.json()["id"],
                     name=response.json()["name"],
-                    created_at=pendulum.parse(response.json()["created_at"]),
-                    updated_at=pendulum.parse(response.json()["updated_at"])
+                    created_at=created_at,
+                    updated_at=updated_at
                 )
-        except httpx.HTTPStatusError as e:
+        except httpx.HTTPStatusError:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Có lỗi xảy ra phía dịch vụ sản phẩm")
         except (httpx.ConnectError, httpx.TimeoutException, httpx.RequestError):
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Dịch vụ sản phẩm không khả dụng')
-            
